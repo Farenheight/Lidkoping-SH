77c2d67dbc9cd8c6dd889e372579bd45bf6ef37e
Robin Grönberg <robgro@telia.com>
2013-09-10 21:09:28 +0200

    Added backbutton to Activity field on the main activity.

--- src/com/example/lidkopingsh/MainActivity.java
+++ src/com/example/lidkopingsh/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lidkopingsh;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ActionBar actionbar = getActionBar();
+		actionbar.setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
@@ -17,6 +23,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		Log.d("DEBUG", "Clicked ffs");
+		return super.onOptionsItemSelected(item);
 	}
 
 }
