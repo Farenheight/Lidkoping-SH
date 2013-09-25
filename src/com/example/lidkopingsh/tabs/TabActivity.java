65203a5988ebff7313cc36a617e93391e4890e23
Simon Bengtsson <simonbengt@gmail.com>
2013-09-25 17:44:31 +0200

    Loaded in tabs the new way

--- src/com/example/lidkopingsh/tabs/TabActivity.java
+++ src/com/example/lidkopingsh/tabs/TabActivity.java
@@ -6,26 +6,23 @@ import android.widget.TabHost;
 
 import com.example.lidkopingsh.R;
 
-public class TabExample extends Activity {
+public class TabActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
+		setContentView(R.layout.tabs_layout);
 		TabHost tabs = (TabHost) findViewById(R.id.TabHost01);
-
 		tabs.setup();
-
 		TabHost.TabSpec spec1 = tabs.newTabSpec("tag1");
-
-		spec1.setContent(R.id.AnalogClock01);
-		spec1.setIndicator("Analog Clock");
+		
+		spec1.setContent(R.id.tabDrawingContainer);
+		spec1.setIndicator("Analog Clock"); 
 
 		tabs.addTab(spec1);
 
 		TabHost.TabSpec spec2 = tabs.newTabSpec("tag2");
-		spec2.setContent(R.id.DigitalClock01);
+		spec2.setContent(R.id.tabDetailContainer);
 		spec2.setIndicator("Digital Clock");
 
 		tabs.addTab(spec2);
