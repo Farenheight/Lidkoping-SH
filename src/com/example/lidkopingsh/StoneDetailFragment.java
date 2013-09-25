bb2aaac0595f6414e067958703bdba4e1f643b4e
Simon Bengtsson <simonbengt@gmail.com>
2013-09-25 18:19:18 +0200

    Moved the tab view to StoneDetailFragment from tmp TabActivity

--- src/com/example/lidkopingsh/StoneDetailFragment.java
+++ src/com/example/lidkopingsh/StoneDetailFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 /**
  * A fragment representing a single Stone detail screen. This fragment is either
@@ -28,13 +29,26 @@ public class StoneDetailFragment extends Fragment {
 	public StoneDetailFragment() {
 	}
 
-
-
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) { 
-		View rootView = inflater.inflate(R.layout.fragment_stone_detail,
+		View rootView = inflater.inflate(R.layout.tabs_layout,
 				container, false);
+				
+		TabHost tabHost = (TabHost) rootView.findViewById(R.id.orderTabHost);
+		tabHost.setup();
+		
+		//Add drawing tab
+		TabHost.TabSpec drawingTab = tabHost.newTabSpec("drawingTab");
+		drawingTab.setContent(R.id.tabDrawingContainer);
+		drawingTab.setIndicator("Ritning"); 
+		tabHost.addTab(drawingTab);
+
+		//Add detail tab
+		TabHost.TabSpec detailTab = tabHost.newTabSpec("detailTab");
+		detailTab.setContent(R.id.tabDetailContainer);
+		detailTab.setIndicator("Detaljer");
+		tabHost.addTab(detailTab);
 
 
 		return rootView;
