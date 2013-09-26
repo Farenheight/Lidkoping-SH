1fe10c6117dbddce534899f401a1b702fe517f37
Kim Kling <kim.kling@hotmail.com>
2013-09-26 13:28:36 +0200

    Fixed misspelling on cemetary

--- src/com/example/lidkopingsh/database/OrderDbStorage.java
+++ src/com/example/lidkopingsh/database/OrderDbStorage.java
@@ -92,7 +92,7 @@ class OrderDbStorage {
 		values.put(OrderTable.COLUMN_NAME_CUSTOMER_ID, order.getCustomer()
 				.getId());
 		values.put(OrderTable.COLUMN_NAME_ID_NAME, order.getIdName());
-		values.put(OrderTable.COLUMN_NAME_CEMETERY, order.getCementary());
+		values.put(OrderTable.COLUMN_NAME_CEMETERY, order.getCemetary());
 		values.put(OrderTable.COLUMN_NAME_TIME_CREATED, order.getTimeCreated());
 		values.put(OrderTable.COLUMN_NAME_TIME_LAST_UPDATE,
 				order.getLastTimeUpdate());
