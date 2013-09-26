ae85f648cb3ed71899f80888838c007d5b03f1ff
Olliver <olliver@student.chalmers.se>
2013-09-26 16:31:34 +0200

    Started implementing SearchFilter

---
+++ src/com/example/lidkopingsh/SearchFilter.java
@@ -0,0 +1,22 @@
+package com.example.lidkopingsh;
+
+import android.widget.Filter;
+
+public class SearchFilter extends Filter {
+
+	public SearchFilter(){
+		
+	}
+	@Override
+	protected FilterResults performFiltering(CharSequence arg0) {
+		
+		return null;
+	}
+
+	@Override
+	protected void publishResults(CharSequence arg0, FilterResults arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/example/lidkopingsh/TasksAdapter.java
+++ src/com/example/lidkopingsh/TasksAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import com.example.lidkopingsh.model.Order;
@@ -64,6 +65,10 @@ public class TasksAdapter extends ArrayAdapter<Order> {
 		text.setText(clickedOrder.getIdName());
 
 		return view;
+	}
+	public Filter getFilter(){
+		return null;
+		
 	}
 
 }
