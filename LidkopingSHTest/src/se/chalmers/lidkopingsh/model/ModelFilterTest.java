1c56d5a99ff9a64856694f1ee56e6e641fd0554a
Simon Bengtsson <simonbengt@gmail.com>
2013-09-27 17:22:49 +0200

    Added a lot of test cases for ModelFilter

--- LidkopingSHTest/src/se/chalmers/lidkopingsh/model/ModelFilterTest.java
+++ LidkopingSHTest/src/se/chalmers/lidkopingsh/model/ModelFilterTest.java
@@ -1,17 +1,170 @@
 package se.chalmers.lidkopingsh.model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ModelFilterTest {
+	private List<Order> orders;
+	private IModelFilter<Order> filter;
 
+	@Before
+	public void setUp() {
+		filter = new ModelFilter();
+		orders = new ArrayList<Order>();
+	}
+
+	/**
+	 * Filter should give no results if id name is not matching the constraint
+	 */
 	@Test
-	public void test() {
-		IModelFilter<Order> filter = new ModelFilter();
+	public void testNoResults() {
+		String testIdName = "O.R";
+		String testConstraint = "Not a matching string";
+
+		orders.add(new Order(0, null, testIdName, 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 0, is:"
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 0);
+	}
+	
+	/**
+	 * Filter should match part of constraint
+	 */
+	@Test
+	public void testPartOfConstraint() {
+		String testIdName = "O.R";
+		String testConstraint = "O";
+
+		orders.add(new Order(0, null, testIdName, 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 1, is:"
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 1);
+	}
+	
+	/**
+	 * Filter should only match beginning of constraint
+	 */
+	@Test
+	public void testNotInTheBeginning() {
+		String testIdName = "O.SS";
+		String testConstraint = "SS";
+
+		orders.add(new Order(0, null, testIdName, 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 0, is:"
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 0);
+	}
+
+	/**
+	 * Filter should be letter case independent
+	 */
+	@Test
+	public void testLetterCase() {
+		String testIdName = "O.R";
+		String testConstraint = "o.r";
+
+		orders.add(new Order(0, null, testIdName, 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 1, is:"
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 1);
+	}
+
+	/**
+	 * Filter should not care about dots
+	 */
+	@Test
+	public void testDotIndependece() {
+		String testIdName = "O.R";
+		String testConstraint = "or";
+
+		orders.add(new Order(0, null, testIdName, 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 1, is:"
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 1);
+	}
+	
+	/**
+	 * Empty string should match everything
+	 */
+	@Test
+	public void testEmptyString() {
+		String testConstraint = "";
+
+		orders.add(new Order(0, null, "O.R", 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		orders.add(new Order(0, null, "O.S", 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		orders.add(new Order(0, null, "S.S", 0, 0, null, null, null, null,
+				0, new Customer(), null));
 		
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 3, is: "
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 3);
+	}
+	
+	/**
+	 * Null should match everything
+	 */
+	@Test
+	public void testNull() {
+		String testConstraint = null;
+
+		orders.add(new Order(0, null, "O.R", 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		orders.add(new Order(0, null, "O.S", 0, 0, null, null, null, null,
+				0, new Customer(), null));
+		orders.add(new Order(0, null, "S.S", 0, 0, null, null, null, null,
+				0, new Customer(), null));
 		
-		assertTrue(true);
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 3, is: "
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 3);
+	}
+	
+	/**
+	 * Emty Order list should return nothing
+	 */
+	@Test
+	public void testEmptyOrderList() {
+		String testConstraint = null;
+		
+		List<Order> returnedOrderList = filter.getOrdersByFilter(
+				testConstraint, orders);
+
+		String assertMessage = "Size of list should be 0, is: "
+				+ returnedOrderList.size();
+		assertTrue(assertMessage, returnedOrderList.size() == 0);
 	}
 
 }
