f17e33de02d22086cdaa51ca68b4b99e194d4cfc
Alexander Härenstam <alehar@student.chalmers.se>
2013-10-02 15:29:20 +0200

    Created IServerLayer and ServerLayer.

---
+++ src/se/chalmers/lidkopingsh/database/IServerLayer.java
@@ -0,0 +1,11 @@
+package se.chalmers.lidkopingsh.database;
+/**
+ * Interface for a layer between Server and Local database.
+ * 
+ * @author Alexander Härenstam
+ * @author Olliver Mattsson
+ *
+ */
+public interface IServerLayer {
+
+}
