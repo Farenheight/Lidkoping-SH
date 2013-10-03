1f1d65c592d213c3d3f018e69f0eeb33454d7767
Kim Kling <kim.kling@hotmail.com>
2013-10-03 17:52:44 +0200

    Improved mySQLConnection

--- api/class/mySQLConnection.php
+++ api/class/mySQLConnection.php
@@ -4,38 +4,29 @@ class mySQLConnection {
 	private $con;
 	private $stmt;
 
-	public function mySQLConnection($host, $username, $password, $db) {
-		$this -> con = new mysqli($GLOBALS['db_host'], $GLOBALS['db_user'], $GLOBALS['db_password'], $GLOBALS['db_database']);
-		if ($mysqli -> connect_errno) {
+	public function mySQLConnection() {
+		$this -> con = new mysqli($GLOBALS['db_host'], $GLOBALS['db_username'], $GLOBALS['db_password'], $GLOBALS['db_database']);
+		if ($this -> con -> connect_errno) {
 			errorSql("Failed to connect", $mysqli -> connect_errno, $mysqli -> connect_error);
 		}
 		$this -> con -> autocommit(FALSE);
 	}
 
 	public function commit() {
-		if(!$this -> con -> commit()){
-			errorSql("Commit failed", $mysqli -> errno, $mysqli -> error);
-		}
-	}
-
-	public function prepare($statement) {
-		if (!($this -> stmt = $mysqli -> prepare($statement))) {
-			errorSql("Prepare failed", $mysqli -> errno, $mysqli -> error);
-		}
-	}
-
-	public function bind_param($type, $value) {
-		if (!$this->stmt -> bind_param($type, $value)) {
-			errorSql("Binding parameters failed", $this->stmt->errno, $this->stmt->error);
+		if (!$this -> con -> commit()) {
+			errorSql("Commit failed", $this -> con -> errno, $this -> con -> error);
 		}
 	}
 	
-	public function execute(){
-		if (!$this->stmt->execute()) {
-			errorSql("Execute failed", $this->stmt->errno, $this->stmt->error);
+	public function escape($string){
+		return $this->con->escape_string($string);
+	}
+
+	public function prepare($statement) {
+		if (!($this -> stmt = $this -> con -> prepare($statement))) {
+			errorSql("Prepare failed", $this -> con -> errno, $this -> con -> error);
 		}
-		//$this->stmt->close();
-		return $this->stmt;
+		return $this -> stmt;
 	}
 }
 ?>
