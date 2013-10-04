bfb97a55ad69c956207eb70331df8e5e9240ca31
Robin Grönberg <robgro@telia.com>
2013-10-04 16:36:28 +0200

    Fixed bug that added wrong Order to orderDetails view when searching for an Order with a String

--- src/se/chalmers/lidkopingsh/OrderListFragment.java
+++ src/se/chalmers/lidkopingsh/OrderListFragment.java
@@ -192,7 +192,7 @@ public class OrderListFragment extends ListFragment {
 		super.onListItemClick(listView, view, position, id);
 		// Notify the active callbacks interface (the activity, if the
 		// fragment is attached to one) that an item has been selected.
-		mCallbacks.onItemSelected(mOrderList.get(position - 1).getId());
+		mCallbacks.onItemSelected(mOrderAdapter.getItem(position - 1).getId());
 	}
 
 	@Override
