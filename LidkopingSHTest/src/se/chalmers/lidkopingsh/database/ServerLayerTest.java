e6e2db17ae38f73f08a1ab72942234d59ab0a0ee
Olliver <olliver@student.chalmers.se>
2013-10-04 15:24:20 +0200

    Attempts to fix noclassdeffounderror

--- LidkopingSHTest/src/se/chalmers/lidkopingsh/database/ServerLayerTest.java
+++ LidkopingSHTest/src/se/chalmers/lidkopingsh/database/ServerLayerTest.java
@@ -7,35 +7,22 @@ import org.junit.Test;
 import se.chalmers.lidkopingsh.model.Order;
 import android.test.AndroidTestCase;
 
-/**
- *  
- * @author Alexander Härenstam
- * @author Olliver Mattsson
- */
-public class ServerLayerTest extends AndroidTestCase {
-
-	private OrderDbStorage dbStorage;
-	private static Collection<Order> originalData;
+public class ServerLayerTest extends AndroidTestCase{
+	OrderDbStorage dbStorage;
 
 	@Override
 	protected void setUp() throws Exception {
 		dbStorage = new OrderDbStorage(this.getContext());
 		dbStorage.clear();
-
-		originalData = new OrderDbStorage(this.getContext()).query(null, null,
-				null);
-
-		super.setUp();
 	}
-
+	
 	@Test
-	public void testGetUpdates() {
-		ServerLayer serverLayer = new ServerLayer(
-				"http://lidkopingsh.kimkling.net/api/", this.getContext());
+	public void testGetUpdate(){
+		ServerLayer serverLayer = new ServerLayer("http://lidkopingsh.kimkling.net/api/", this.getContext());
 		Collection<Order> ordersBefore = dbStorage.query(null, null, null);
-		Collection<Order> ordersAfter = serverLayer.getUpdates();
+		Order[] ordersAfter = serverLayer.getUpdates();
 		
-		assertTrue(ordersBefore.size() < ordersAfter.size());
+		assertTrue(ordersBefore.size() < ordersAfter.length);
 	}
 
 }
