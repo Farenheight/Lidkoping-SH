a749e0934dd8a6e381b9bb6806256c71e16d02e0
Robin Grönberg <robgro@telia.com>
2013-10-08 12:37:07 +0200

    Test

---
+++ src/se/chalmers/lidkopingsh/OrderMapFragement.java
@@ -0,0 +1,13 @@
+package se.chalmers.lidkopingsh;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.MapFragment;
+
+public class OrderMapFragement extends MapFragment {
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		//TODO work here next time hehe
+	}
+}
