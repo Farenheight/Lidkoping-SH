6b3e6e37c18b6db43db6edbe8e3fec978b7e31f3
Simon Bengtsson <simonbengt@gmail.com>
2013-10-08 16:56:45 +0200

    Added a pin and zoomed to Sweden

--- src/se/chalmers/lidkopingsh/OrderMapActivity.java
+++ src/se/chalmers/lidkopingsh/OrderMapActivity.java
@@ -1,67 +1,93 @@
 package se.chalmers.lidkopingsh;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 /**
  * Activity containing a map with all the orders positions marked.
  */
 public class OrderMapActivity extends FragmentActivity {
-    /**
-     * Note that this may be null if the Google Play services APK is not available.
-     */
-    private GoogleMap mMap;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_root);
-        setUpMapIfNeeded();
-    }
+	private final static String TAG = "Debug tag";
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+	/**
+	 * Note that this may be null if the Google Play services APK is not
+	 * available.
+	 */
+	private GoogleMap mMap;
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not have been
-     * completely destroyed during this process (it is likely that it would only be stopped or
-     * paused), {@link #onCreate(Bundle)} may not be called again so we should call this method in
-     * {@link #onResume()} to guarantee that it will be called.
-     */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map_root);
+		setUpMapIfNeeded();
+	}
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera.
-     */
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
+
+	/**
+	 * Sets up the map if it is possible to do so (i.e., the Google Play
+	 * services APK is correctly installed) and the map has not already been
+	 * instantiated.. This will ensure that we only ever call
+	 * {@link #setUpMap()} once when {@link #mMap} is not null.
+	 * <p>
+	 * If it isn't installed {@link SupportMapFragment} (and
+	 * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt
+	 * for the user to install/update the Google Play services APK on their
+	 * device.
+	 * <p>
+	 * A user can return to this FragmentActivity after following the prompt and
+	 * correctly installing/updating/enabling the Google Play services. Since
+	 * the FragmentActivity may not have been completely destroyed during this
+	 * process (it is likely that it would only be stopped or paused),
+	 * {@link #onCreate(Bundle)} may not be called again so we should call this
+	 * method in {@link #onResume()} to guarantee that it will be called.
+	 */
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	/**
+	 * This is where we can add markers or lines, add listeners or move the
+	 * camera.
+	 */
+	private void setUpMap() {
+		mMap.moveCamera(CameraUpdateFactory
+				.newCameraPosition(new CameraPosition.Builder()
+						.target(new LatLng(58, 15)).zoom(5).build()));
+		mMap.getUiSettings().setZoomControlsEnabled(false);
+		mMap.addMarker(new MarkerOptions().position(new LatLng(58, 15)).title(
+				"Lidköpings Stenhuggeri"));
+	}
 }
