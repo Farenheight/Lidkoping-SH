1080d3c6ea6ed6c78ef98f361c1bd40bd8571d4f
Simon Bengtsson <simonbengt@gmail.com>
2013-10-09 15:38:40 +0200

    Added orders to map

--- src/se/chalmers/lidkopingsh/OrderMapActivity.java
+++ src/se/chalmers/lidkopingsh/OrderMapActivity.java
@@ -1,19 +1,37 @@
 package se.chalmers.lidkopingsh;
 
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_NORMAL;
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_SATELLITE;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import se.chalmers.lidkopingsh.handler.ModelHandler;
+import se.chalmers.lidkopingsh.model.IModel;
+import se.chalmers.lidkopingsh.model.Order;
+import android.annotation.SuppressLint;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
@@ -29,10 +47,18 @@ public class OrderMapActivity extends FragmentActivity {
 	 */
 	private GoogleMap mMap;
 
+	private List<Marker> mMarkers;
+
+	private IModel mModel;
+
+	private Geocoder mGeoCoder;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_root);
+		mModel = ModelHandler.getModel(this);
+		mMarkers = new ArrayList<Marker>();
 		setUpMapIfNeeded();
 	}
 
@@ -75,27 +101,145 @@ public class OrderMapActivity extends FragmentActivity {
 	}
 
 	/**
+	 * Get markers
+	 */
+
+	/**
 	 * This is where we can add markers or lines, add listeners or move the
 	 * camera.
 	 */
 	private void setUpMap() {
-		Geocoder gc = new Geocoder(this);
-		List<Address> aList = null;
-		try {
-			aList = gc.getFromLocationName("Örslösa kyrkogård", 5);
-		} catch (IOException e) {
-			Log.e("DEBUG", "Error reading Google Maps locations....");
-			e.printStackTrace();
+		initMapTypeSpinner();
+		addMarkers();
+		addDebugMarkers();
+		zoomToMarkers(100);
+	}
+
+	private void initMapTypeSpinner() {
+		Spinner spinner = (Spinner) findViewById(R.id.layers_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.layers_array,
+				android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				setLayer((String) parent.getItemAtPosition(position));
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+
+			private void setLayer(String layerName) {
+				if (layerName.equals(getString(R.string.normal))) {
+					mMap.setMapType(MAP_TYPE_NORMAL);
+				} else if (layerName.equals(getString(R.string.satellite))) {
+					mMap.setMapType(MAP_TYPE_SATELLITE);
+				} else {
+					Log.i("LDA", "Error setting layer with name " + layerName);
+				}
+			}
+		});
+	}
+
+	// Adds ten markers in stockholm TODO: Remove
+	private void addDebugMarkers() {
+		Address cAddress = getAddress("eggvena kyrka");
+		LatLng cLatLng = new LatLng(cAddress.getLatitude(),
+				cAddress.getLongitude());
+		Marker cemeteryMarker = mMap.addMarker(new MarkerOptions()
+				.position(cLatLng));
+		mMarkers.add(cemeteryMarker);
+	}
+
+	/**
+	 * Add one marker for every order in the model's order list
+	 */
+	private void addMarkers() {
+		double debugEps = 0; // TODO: Remove
+		for (Order order : mModel.getOrders()) {
+			Address cAddress = getAddress(formatCemeteryName(order
+					.getCemetary()));
+			LatLng cLatLng = new LatLng(cAddress.getLatitude() + debugEps,
+					cAddress.getLongitude() + debugEps);
+			Marker cemeteryMarker = mMap.addMarker(new MarkerOptions()
+					.position(cLatLng).title(order.getCemetary()));
+			mMarkers.add(cemeteryMarker);
+			debugEps += 0.01;
 		}
-		Address a = aList.get(0);
-		mMap.moveCamera(CameraUpdateFactory
-				.newCameraPosition(new CameraPosition.Builder()
-						.target(new LatLng(58, 15)).zoom(6).build()));
-		mMap.moveCamera(CameraUpdateFactory
-				.newCameraPosition(new CameraPosition.Builder()
-						.target(new LatLng(52, 15)).zoom(6).build()));
-		mMap.getUiSettings().setZoomControlsEnabled(false);
-		mMap.addMarker(new MarkerOptions().position(new LatLng(a.getLatitude(), a.getLongitude())).title(
-				"Lidköpings Stenhuggeri"));
+	}
+
+	/**
+	 * @param searchTerm
+	 *            The term to search for
+	 * @return Address object with a latitude and longitude among other things
+	 */
+	private Address getAddress(String searchTerm) {
+		if (mGeoCoder == null) {
+			mGeoCoder = new Geocoder(this);
+		}
+		try {
+			List<Address> aList = mGeoCoder.getFromLocationName(searchTerm, 1);
+			return aList.get(0);
+		} catch (IOException e) {
+			Log.e("DEBUG",
+					"Network error in getting lat and long from cemetery name");
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private String formatCemeteryName(String cemeteryName) {
+		// TODO: Handle if cemetery is null
+		cemeteryName = cemeteryName.toLowerCase();
+		if (cemeteryName.contains("kyrkogård")
+				|| cemeteryName.contains("kyrka")) {
+			return cemeteryName;
+		} else {
+			return cemeteryName + "kyrka";
+		}
+	}
+
+	/**
+	 * Zoom to the added markers
+	 * 
+	 * @param padding
+	 *            The padding in pixels around the markers
+	 */
+	private void zoomToMarkers(final int padding) {
+		// Pan to see all markers in view.
+		// Cannot zoom to bounds until the map has a size.
+		final View mapView = getSupportFragmentManager().findFragmentById(
+				R.id.map).getView();
+		if (mapView.getViewTreeObserver().isAlive()) {
+			mapView.getViewTreeObserver().addOnGlobalLayoutListener(
+					new OnGlobalLayoutListener() {
+						@SuppressWarnings("deprecation")
+						// We use the new method when supported
+						@SuppressLint("NewApi")
+						// We check which build version we are using.
+						@Override
+						public void onGlobalLayout() {
+							LatLngBounds.Builder bld = new LatLngBounds.Builder();
+							for (int i = 0; i < mMarkers.size(); i++) {
+								bld.include(mMarkers.get(i).getPosition());
+							}
+							LatLngBounds bounds = bld.build();
+							if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+								mapView.getViewTreeObserver()
+										.removeGlobalOnLayoutListener(this);
+							} else {
+								mapView.getViewTreeObserver()
+										.removeOnGlobalLayoutListener(this);
+							}
+							mMap.moveCamera(CameraUpdateFactory
+									.newLatLngBounds(bounds, padding));
+						}
+					});
+		}
 	}
 }
