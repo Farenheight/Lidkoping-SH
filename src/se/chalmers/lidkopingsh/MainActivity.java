a989e845b789e103f73f9b7435e75cb9fd3c3a36
Simon Bengtsson <simonbengt@gmail.com>
2013-10-14 19:38:59 +0200

    Refined GUI

--- src/se/chalmers/lidkopingsh/MainActivity.java
+++ src/se/chalmers/lidkopingsh/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends FragmentActivity implements
 		super.onCreate(savedInstanceState);
 
 		mTabletSize = getResources().getBoolean(R.bool.isTablet);
+		mTabletSize = true;
 		if (mTabletSize) {
 			setContentView(R.layout.tablet_maincontainer);
 			((OrderListFragment) getSupportFragmentManager().findFragmentById(
