437f7c6475186c6ec9c5722c504e63051b919e19
Anton Jansson <antonajgarn@gmail.com>
2013-10-15 18:13:59 +0200

    Added method for retrieving web api key in ServerLayer.

--- src/se/chalmers/lidkopingsh/handler/AbstractServerLayer.java
+++ src/se/chalmers/lidkopingsh/handler/AbstractServerLayer.java
@@ -2,6 +2,7 @@ package se.chalmers.lidkopingsh.handler;
 
 import java.util.List;
 
+import se.chalmers.lidkopingsh.handler.ServerLayer.ResponseSend;
 import se.chalmers.lidkopingsh.model.Order;
 
 /**
@@ -9,22 +10,37 @@ import se.chalmers.lidkopingsh.model.Order;
  * 
  * @author Alexander H�renstam
  * @author Olliver Mattsson
- *
+ * 
  */
 public abstract class AbstractServerLayer {
 	protected String serverPath;
+
+	/**
+	 * Tries to authenticate and retrieve an API key for this device.
+	 * 
+	 * @param username
+	 * @param password
+	 * @param deviceId
+	 *            Unique device id.
+	 * @return Response with success status, error code and message. API key is
+	 *         returned in message if it exists.
+	 */
+	public abstract ResponseSend getApikey(String username, String password,
+			String deviceId);
+
 	/**
 	 * Retrieve updates from server.
 	 */
 	public abstract List<Order> getUpdates(boolean getAll);
-	
+
 	/**
 	 * Send updates to server.
 	 */
 	public abstract boolean sendUpdate(Order order);
-	
+
 	/**
 	 * Get path to server.
+	 * 
 	 * @return String serverPath
 	 */
 	public String getServerPath() {
--- src/se/chalmers/lidkopingsh/handler/ServerLayer.java
+++ src/se/chalmers/lidkopingsh/handler/ServerLayer.java
@@ -1,8 +1,10 @@
 package se.chalmers.lidkopingsh.handler;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +15,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
 
 import se.chalmers.lidkopingsh.model.Order;
@@ -24,8 +27,9 @@ import com.google.gson.Gson;
 /**
  * Handling communication between remote server and local database.
  * 
- * @author Alexander H�renstam
+ * @author Alexander Härenstam
  * @author Olliver Mattsson
+ * @author Anton Jansson
  * 
  */
 public class ServerLayer extends AbstractServerLayer {
@@ -46,7 +50,6 @@ public class ServerLayer extends AbstractServerLayer {
 			Log.e("server_layer",
 					"Error in HTTP Server Connection" + e.toString());
 		}
-
 	}
 
 	/**
@@ -55,14 +58,27 @@ public class ServerLayer extends AbstractServerLayer {
 	 * @param orderString
 	 */
 	private BufferedReader sendHttpPostRequest(String orderString, String action) {
+		return sendHttpPostRequest(orderString, action,
+				Arrays.asList(new BasicHeader("LidkopingSH-Authentication",
+						"123456789qwertyuiop")));
+	}
+
+	/**
+	 * Used to send a POST request to server.
+	 * 
+	 * @param orderString
+	 */
+	private BufferedReader sendHttpPostRequest(String orderString,
+			String action, Collection<? extends Header> headers) {
 		BufferedReader reader = null;
 		try {
 			HttpPost httpPost = new HttpPost(serverPath + action);
 			httpPost.setEntity(new StringEntity(orderString, HTTP.UTF_8));
-			httpPost.setHeader("LidkopingSH-Authentication",
-					"123456789qwertyuiop");
 			httpPost.setHeader("Content-Type",
 					"application/x-www-form-urlencoded");
+			for (Header h : headers) {
+				httpPost.setHeader(h);
+			}
 			HttpResponse httpResponse = httpClient.execute(httpPost);
 			reader = new BufferedReader(new InputStreamReader(httpResponse
 					.getEntity().getContent()));
@@ -80,15 +96,11 @@ public class ServerLayer extends AbstractServerLayer {
 	 *            A JsonObject with the ids and timestamps for comparing orders
 	 */
 	private List<Order> getUpdatedOrdersFromServer(String orderVerifiers) {
-		BufferedReader reader = sendHttpPostRequest("data=" + orderVerifiers, "getUpdates");
+		BufferedReader reader = sendHttpPostRequest("data=" + orderVerifiers,
+				"getUpdates");
 
-		ResponseGet response = null;
-		try {
-			response = new Gson().fromJson(reader, ResponseGet.class);
-		} catch (Exception e) {
-			Log.d("server_layer", "No data from server");
-		}
-		
+		ResponseGet response = getResponseGet(reader);
+
 		if (response.isSuccess()) {
 			List<Order> ord = new LinkedList<Order>();
 			for (Order o : response.getResults()) {
@@ -99,6 +111,18 @@ public class ServerLayer extends AbstractServerLayer {
 			printErrorLog(response);
 		}
 		return null;
+	}
+
+	@Override
+	public ResponseSend getApikey(String username, String password,
+			String deviceId) {
+		Collection<Header> headers = new ArrayList<Header>();
+		headers.add(new BasicHeader("Lidkopingsh-Username", username));
+		headers.add(new BasicHeader("Lidkopingsh-Password", password));
+		headers.add(new BasicHeader("Lidkopingsh-Deviceid", deviceId));
+		BufferedReader reader = sendHttpPostRequest("", "getApikey", headers);
+
+		return getResponseGet(reader);
 	}
 
 	@Override
@@ -123,13 +147,8 @@ public class ServerLayer extends AbstractServerLayer {
 		Gson gsonOrder = new Gson();
 		String json = "data=" + gsonOrder.toJson(order);
 		BufferedReader reader = sendHttpPostRequest(json, "postOrder");
-		
-		ResponseSend response = null;
-		try {
-			response = new Gson().fromJson(reader, ResponseSend.class);
-		} catch (Exception e) {
-			Log.d("server_layer", "No data from server");
-		}
+
+		ResponseSend response = getResponseSend(reader);
 
 		if (!response.isSuccess()) {
 			order.sync(null); // Informing that no data has been able to change.
@@ -137,6 +156,26 @@ public class ServerLayer extends AbstractServerLayer {
 			return response.isSuccess();
 		}
 		return response.isSuccess();
+	}
+	
+	private ResponseGet getResponseGet(Reader reader) {
+		ResponseGet response = null;
+		try {
+			response = new Gson().fromJson(reader, ResponseGet.class);
+		} catch (Exception e) {
+			Log.d("server_layer", "No data from server");
+		}
+		return response;
+	}
+	
+	private ResponseSend getResponseSend(Reader reader) {
+		ResponseSend response = null;
+		try {
+			response = new Gson().fromJson(reader, ResponseSend.class);
+		} catch (Exception e) {
+			Log.d("server_layer", "No data from server");
+		}
+		return response;
 	}
 
 	private void printErrorLog(ResponseSend response) {
@@ -146,17 +185,17 @@ public class ServerLayer extends AbstractServerLayer {
 
 	private class ResponseGet extends ResponseSend {
 		private List<Order> results;
-		
+
 		public List<Order> getResults() {
 			return results;
 		}
 	}
-	
-	private class ResponseSend {
+
+	public class ResponseSend {
 		private boolean success;
 		private int errorcode;
 		private String message;
-		
+
 		public boolean isSuccess() {
 			return success;
 		}
