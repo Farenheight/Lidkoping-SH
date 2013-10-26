10a4484aab3982104f10ff52cf30a2fea69b1d49
Kim Kling <kim.kling@hotmail.com>
2013-10-26 15:57:01 +0200

    Added small changes

--- api/db_config.php
+++ api/db_config.php
@@ -5,6 +5,14 @@ $db_database = "";
 $db_username = "";
 $db_password = "";
 
-// API key, for testing
-$test_apiKey = "";
+// Peppers
+$userPepper = "";
+$apikeyPepper = "";
+$pwHashingConstant = 0;
+
+// Installation specific urls and apikey
+$insertUrl = "http://url/api/?action=insertOrder";
+$insertApikey = "Lidkopingsh-Apikey: 000000\r\n";
+$insertDeviceId = "Lidkopingsh-Deviceid: aaaaaa\r\n";
+
 ?>
